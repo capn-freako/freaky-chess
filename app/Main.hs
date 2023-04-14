2726c46070edfd8ab4f16d265e30498845e87a42
David Banas <capn.freako@gmail.com>
2023-04-14 15:30:13 -0400

    Implemented checkmate detection. (#9)
    Fixes #7

--- app/Main.hs
+++ app/Main.hs
@@ -19,20 +19,36 @@ import Chess.Types
 
 main :: IO ()
 main = do
-  moves <- flip unfoldM newGame $ \brd -> do
+  moves <- flip unfoldM (0, newGame) $ \(score, brd) -> do
     printBoard brd
-    putStr "Move? "
-    hFlush stdout
-    cmd <- getLine
-    case evalCmd brd cmd of
-      Left  msg  -> case msg of
-                      "quit" -> return Nothing
-                      _      -> do putStrLn msg
-                                   return $ Just (rankBoard brd, brd)
-      Right brd' -> case rankMoves Blk brd' of
-                      []       -> do putStrLn "Black cannot move!"
-                                     return Nothing
-                      best : _ -> return $ Just best
+    putStrLn $ "Score: " ++ show score
+    -- Check for mate.
+    case rankMoves Wht brd of
+      []       -> do putStrLn "White cannot move!"
+                     return Nothing
+      -- ToDo: This won't detect a stalemate.
+      best : _ -> let scoreChange = fst best - score
+                   in if scoreChange > 9000
+                        then do putStrLn "White wins!"
+                                return Nothing
+                        else if scoreChange < -9000
+                               then do putStrLn $ "Black wins!"
+                                       return Nothing
+                               else do putStr "Move? "  -- Parse, validate, and execute White's move.
+                                       hFlush stdout
+                                       cmd <- getLine
+                                       case evalCmd brd cmd of
+                                         Left  msg  -> case msg of
+                                                         "quit" -> return Nothing
+                                                         _      -> do putStrLn msg
+                                                                      return $ Just (score, (score, brd))
+                                         -- If White's move is valid then calculate Black's response.
+                                         Right brd' -> case rankMoves Blk brd' of
+                                                         []        -> do putStrLn "Black cannot move!"
+                                                                         return Nothing
+                                                         best' : _ -> do let brd''  = snd best'
+                                                                             score' = rankBoard brd''
+                                                                         return $ Just (score', (score', brd''))
   putStrLn "Finished. Score history:"
   forM_ moves print
 
